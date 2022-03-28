f10e0318e816fb2826ff60c7d6ac4560cfd3cf75
Issam <Issam.aboulfadl05@gmail.com>
2022-03-28 18:09:16 +0000

    � Add React Chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -30,14 +30,15 @@ const Featured = () => {
             </div>
           </div>
           <div className="item">
-            <div className="itemTitle">Target</div>
+            <div className="itemTitle">Last Week</div>
+
             <div className="itemResult positive">
               <KeyboardArrowDownIcon fontSize="small" />
               <div className="resultAmount">$12.4k</div>
             </div>
           </div>
           <div className="item">
-            <div className="itemTitle">Target</div>
+            <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
               <KeyboardArrowDownIcon fontSize="small" />
               <div className="resultAmount">$12.4k</div>
