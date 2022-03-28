5428a907fe63a685b34431d56cc7f55f4629f90b
Issam <Issam.aboulfadl05@gmail.com>
2022-03-28 18:41:42 +0000

    React MUI Table Tutorial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,12 +5,13 @@ import Widget from "../../components/widget/Widget";
 import "./home.scss";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">
+      <d iv className="homeContainer">
         <Navbar />
         <div className="widgets">
           <Widget type="user" />
@@ -22,7 +23,11 @@ const Home = () => {
           <Featured />
           <Chart />
         </div>
-      </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+      </d>
     </div>
   );
 };
