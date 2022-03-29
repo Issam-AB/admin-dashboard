b900575d4d4e3d9d9a2ddb775708465cb71a5710
Issam <Issam.aboulfadl05@gmail.com>
2022-03-29 00:25:27 +0000

    add style button in dataTable

---
+++ src/components/dataTable/DataTable.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./dataTable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../dataTableSource";
+
+const DataTable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: "200",
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="dataTable">
+      <DataGrid
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import "./list.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DataTable from "../../components/dataTable/DataTable";
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
