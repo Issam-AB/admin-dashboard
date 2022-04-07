c6fd9861082486438cced059e4c7f4065b170d34
Issam <Issam.aboulfadl05@gmail.com>
2022-04-07 15:31:18 +0000

    ❣ Firebase Crud: How to fetch data?

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./dataTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../dataTableSource";
 import { Link } from "react-router-dom";
-
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase.js";
 const DataTable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setData(list);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
+
   const actionColumn = [
     {
       field: "action",
